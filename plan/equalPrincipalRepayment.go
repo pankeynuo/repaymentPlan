ac7597ed61b00acc1529ddd38e9b42a9854c56f0
jinrong lin <47972734+linjinrongbb@users.noreply.github.com>
2023-12-08 18:01:29 +0800

    Update equalPrincipalRepayment.go

--- plan/equalPrincipalRepayment.go
+++ plan/equalPrincipalRepayment.go
@@ -5,7 +5,7 @@ package main
   *@Author pauline
   *@Date 2023/12/5 10:18
 **/
-func equalPrincipalRepayment(request *RepayPlanRequest, response *RepayPlanResponse) (err error) {
+func equalPrincipalRepayment(request *Request, response *Response) (err error) {
 	/*nextRepayDate := time.Time{}
 	dailyInterestRate := decimal.Decimal{}
 	request.RepayDay, request.PeriodNum, request.LoanStartDate, nextRepayDate, dailyInterestRate, err =
