52e6626b13b48dab2690b0750ee45dec079fc960
jinrong lin <47972734+linjinrongbb@users.noreply.github.com>
2023-12-08 17:59:40 +0800

    Update billDateRepayment.go

--- plan/billDateRepayment.go
+++ plan/billDateRepayment.go
@@ -5,7 +5,7 @@ package main
   *@Author pauline
   *@Date 2023/12/5 10:19
 **/
-func billDateRepayment(request *RepayPlanRequest, response *RepayPlanResponse) (err error) {
+func billDateRepayment(request *Request, response *Response) (err error) {
 	// check
 	/*if err := checkInterestCalculateStartDateAndEndDate(request.InterestCalculateStartDate, request.InterestCalculateEndDate); nil != err {
 		return nil, err
