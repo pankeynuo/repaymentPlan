9c71c9e4e5f8801c087902135fb647889f08df4c
pauline <47972734+pankeynuo@users.noreply.github.com>
2023-12-11 08:48:42 +0800

    Update equalPrincipalRepayment.go

--- plan/equalPrincipalRepayment.go
+++ plan/equalPrincipalRepayment.go
@@ -5,7 +5,7 @@ package main
   *@Author pauline
   *@Date 2023/12/5 10:18
 **/
-func equalPrincipalRepayment(request *Request, response *Response) (err error) {
+func equalPrincipalRepayment(request *Request) (response *Response, err error) {
 	/*nextRepayDate := time.Time{}
 	dailyInterestRate := decimal.Decimal{}
 	request.RepayDay, request.PeriodNum, request.LoanStartDate, nextRepayDate, dailyInterestRate, err =
@@ -15,5 +15,5 @@ func equalPrincipalRepayment(request *Request, response *Response) (err error) {
 	}*/
 
 	// calculate TODO
-	return nil
+	return nil, nil
 }
