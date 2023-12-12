9b4650cb8371c34d33c49cdbcae0c1117ce285d1
pauline <47972734+pankeynuo@users.noreply.github.com>
2023-12-12 12:47:08 +0800

    Update beforeInterestAfterPrincipal.go

--- plan/beforeInterestAfterPrincipal.go
+++ plan/beforeInterestAfterPrincipal.go
@@ -1,102 +1,69 @@
 package main
 
+import (
+	"github.com/shopspring/decimal"
+)
+
 /**
   *@Description TODO
   *@Author pauline
   *@Date 2023/12/5 10:19
 **/
 func beforeInterestAfterPrincipal(request *Request) (response *Response, err error) {
-	/*// 1.init
-	sumPlanRepayTotalAmount := decimal.Decimal{}
-	sumPlanRepayTotalInterest := decimal.Decimal{}
-	records := make([]models.RepayPlanRecord, 0)
-	flag := false
-	specialStartDate := ""
+	var sumTotalInterest, sumTotalRepayAmount decimal.Decimal
+
+	records := make([]RepayPlanRecord, 0)
+	dateMap := calculatePeriodDate(request)
+
 	// 2.make repay plan
 	for i := 0; i < request.PeriodNum; i++ {
-		log.Debugsf("period = %d", i)
-		// 2.1 get InterestCalculateStartDate,InterestCalculateEndDate,repaymentDate begin
-
-		reCalStartDate := ""
-		reCalEndDate := ""
-		repaymentDate := ""
-		if i == 0 {
-			reCalStartDate = request.InterestCalculateStartDate
-			repaymentDate = nextRepayDate.Format(constant.DATE_DASH_FORMAT)
-		} else {
-			if request.LoanCycleCode == "03" {
-				reCalBeginTs := nextRepayDate.AddDate(0, 0, (i-1)*14)
-				reCalStartDate = time.Unix(reCalBeginTs.Unix(), 0).Format(constant.DATE_DASH_FORMAT)
-				repaymentTs := nextRepayDate.AddDate(0, 0, i*14)
-				repaymentDate = time.Unix(repaymentTs.Unix(), 0).Format(constant.DATE_DASH_FORMAT)
-			}
-			if request.LoanCycleCode == "04" {
-				reCalStartDate, repaymentDate, flag, specialStartDate, err =
-					calcPeriodStartDateAndEndDateByMonthly(i, nextRepayDate, flag, specialStartDate, request.RepayDay)
-				if err != nil {
-					return nil, errors.New(constant.SYSPANIC, err)
-				}
-			}
-			if i == request.PeriodNum-1 {
-				repaymentDate = request.InterestCalculateEndDate
-			}
-		}
-		// 计息结束日=还款日的前一天
-		reCalEndDateTs, _ := time.Parse(constant.DATE_DASH_FORMAT, repaymentDate)
-		reCalEndDate = reCalEndDateTs.AddDate(0, 0, -1).Format(constant.DATE_DASH_FORMAT)
-		// 2.1 get InterestCalculateStartDate,InterestCalculateEndDate,repaymentDate end
-
-		record := models.RepayPlanRecord{
-			PeriodNum:                  i + 1,
-			InterestCalculateStartDate: reCalStartDate,
-			InterestCalculateEndDate:   reCalEndDate,
-			PlanRepayDate:              repaymentDate,
-		}
-
-		// 3.2 get this period's interest begin 当前期次的计息天数
-		days, err := GetTimeInterval(record.InterestCalculateStartDate, record.InterestCalculateEndDate)
-		if err != nil {
-			return nil, errors.New(constant.SYSPANIC, err)
-		}
-		daysOfInterestCal, err := conversionInt(days)
-		if err != nil {
-			return nil, errors.New(constant.SYSPANIC, err)
-		}
+		periodStartDate := dateMap[i][0]
+		periodEndDate := dateMap[i][1]
+		periodRepayDate := dateMap[i][2]
+		// reCalStartDate := ""
+		// reCalEndDate := ""
+		// repaymentDate := ""
+		// 当前期次的计息天数
+		daysOfPeriod := getDaysBetweenDate(periodStartDate, periodEndDate)
 
 		// 当前期次的利息金额=贷款本金*计息天数*日利息
-		planRepayInterest := request.LoanAmount.Mul(rateD).Mul(decimal.NewFromInt(days)).Round(2)
-		log.Debugsf("planRepayInterest = %s", planRepayInterest)
-		// 2.2 get this period's interest end
-		record.DaysOfInterestCalculate = daysOfInterestCal
-		record.PlanRepayInterest = planRepayInterest
+		periodRepayInterest := request.LoanAmount.Mul(rateD).Mul(decimal.NewFromInt(daysOfPeriod)).Round(2)
 
-		// 2.3. calculate the cumulative repayment amount, remaining repayment amount begin
+		record := RepayPlanRecord{
+			PeriodNum:           i + 1,
+			PeriodStartDate:     periodStartDate.Format(DATE_DASH_FORMAT), // 当前期次的开始计息日
+			PeriodEndDate:       periodEndDate.Format(DATE_DASH_FORMAT),   // 当前期次的结束计息日
+			RepayDate:           periodRepayDate.Format(DATE_DASH_FORMAT), // 当前期次的还款日
+			DaysOfPeriod:        int(daysOfPeriod),                        // 当前期次的计息天数
+			PeriodRepayInterest: periodRepayInterest,                      // 当前期次的利息
+		}
+
 		if i == request.PeriodNum-1 {
-			record.PlanRepayPrinciple = request.LoanAmount
-			record.PlanRepayTotalAmount = planRepayInterest.Add(request.LoanAmount)
+			record.PeriodRepayPrinciple = request.LoanAmount
+			record.PeriodRepayTotalAmount = periodRepayInterest.Add(request.LoanAmount)
 			record.MaintainPrinciple = decimal.NewFromInt(0)
 		} else {
-			record.PlanRepayPrinciple = decimal.NewFromFloat(0)
-			record.PlanRepayTotalAmount = planRepayInterest
+			record.PeriodRepayPrinciple = decimal.NewFromFloat(0)
+			record.PeriodRepayTotalAmount = periodRepayInterest
 			record.MaintainPrinciple = request.LoanAmount
 		}
-		// 2.3 alculate the cumulative repayment amount, remaining repayment amount end
-		log.Debugsf("MaintainPrinciple =%s", record.MaintainPrinciple)
-		sumPlanRepayTotalAmount = sumPlanRepayTotalAmount.Add(record.PlanRepayTotalAmount)
-		sumPlanRepayTotalInterest = sumPlanRepayTotalInterest.Add(record.PlanRepayInterest)
+		// 累积还款总金额
+		sumTotalRepayAmount = sumTotalRepayAmount.Add(record.PeriodRepayTotalAmount)
+		// 累积还款总利息
+		sumTotalInterest = sumTotalInterest.Add(record.PeriodRepayInterest)
 
 		records = append(records, record)
 	}
-	response = &models.RepayPlanResponse{
-		Records:                    records,
-		RepayMethod:                request.RepayMethod,
-		InterestCalculateStartDate: request.InterestCalculateStartDate,
-		InterestCalculateEndDate:   request.InterestCalculateEndDate,
-		PeriodNum:                  request.PeriodNum,
-		LoanAmount:                 request.LoanAmount,
-		InterestRate:               request.InterestRate,
-		PlanRepayTotalAmount:       sumPlanRepayTotalAmount,
-		PlanRepayTotalInterest:     sumPlanRepayTotalInterest,
-	}*/
+	response = &Response{
+		PlanRepayRecords: records,
+		RepayMethod:      request.RepayMethod,
+		LoanStartDate:    request.LoanStartDate,
+		LoanEndDate:      request.LoanEndDate,
+		TotalPeriodNum:   request.PeriodNum,
+		LoanAmount:       request.LoanAmount,
+		InterestRate:     request.InterestRate,
+		TotalRepayAmount: sumTotalRepayAmount,
+		TotalInterest:    sumTotalInterest,
+	}
 	return nil, nil
 }
