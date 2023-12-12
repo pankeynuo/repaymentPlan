17924b215403d39c144d32e12b4a9bf2c3a9a470
pauline <47972734+pankeynuo@users.noreply.github.com>
2023-12-12 12:48:39 +0800

    Update calculate_repayment_plan_test.go

--- plan/calculate_repayment_plan_test.go
+++ plan/calculate_repayment_plan_test.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/shopspring/decimal"
 	"testing"
-	"time"
 )
 
 /**
@@ -12,22 +12,20 @@ import (
   *@Date 2023/12/4 11:17
 **/
 func Test_FirstRepayDate(t *testing.T) {
-	a := time.Now()
-	for i := 0; i < 20; i++ {
-		_, _ = CalculateRepaymentPlan(&Request{
-			LoanAmount:    decimal.NewFromFloat(400000),
-			LoanStartDate: "2022-01-01",
-			LoanEndDate:   "",
-			InterestRate:  decimal.NewFromFloat(6),
-			PeriodNum:     5,
-			RepayDay:      10,
-			LoanCycleCode: "03",
-			RepayMethod:   "01", // 01-等额本息
-			PeriodType:    "02", // 01-年 02-月
-			DaysOfYear:    360,
-			DaysOfMonth:   30,
-		})
-	}
-	b := time.Now()
-	t.Log(b.Sub(a))
+	resp, err := CalculateRepaymentPlan(&Request{
+		LoanAmount:    decimal.NewFromFloat(400000),
+		LoanStartDate: "2022-01-01",
+		LoanEndDate:   "",
+		InterestRate:  decimal.NewFromFloat(6),
+		PeriodNum:     5,
+		RepayDay:      10,
+		LoanCycleCode: "03",
+		RepayMethod:   "02", // 01-等额本息 02-等额本金
+		PeriodType:    "02", // 01-年 02-月
+		DaysOfYear:    360,
+		DaysOfMonth:   30,
+	})
+	resp2, _ := json.Marshal(resp)
+	t.Log(string(resp2))
+	t.Log(err)
 }
