1c9d70068eb8c04d874e0216dcb1f537e4d1b922
pauline <47972734+pankeynuo@users.noreply.github.com>
2023-12-13 16:44:32 +0800

    Update calculate_repayment_plan.go

--- plan/calculate_repayment_plan.go
+++ plan/calculate_repayment_plan.go
@@ -84,24 +84,77 @@ func checkPeriodType(periodType string) error {
 }
 
 func getRepaymentPlan(request *Request) (response *Response, err error) {
+
+	var repayPlanRequest repayPlanRequest
+	switch request.RepayMethod {
+	case EqualLoanRepayment, EqualPrincipalRepayment, BeforeInterestAfterPrincipal, EqualPrincipalAndInterest:
+		repayPlanRequest, response, err = prepareGetParameter(request)
+	}
+
 	switch request.RepayMethod {
 	// 01-等额本息
 	case EqualLoanRepayment:
-		response, err = fixedInstallmentMethod(request)
+		err = fixedInstallmentMethodPlan(repayPlanRequest, response)
 	// 02-等额本金
 	case EqualPrincipalRepayment:
-		response, err = fixedPrincipalMethod(request)
+		err = fixedPrincipalMethodPlan(repayPlanRequest, response)
 	// 03-利随本清
 	case BothPrincipalAndInterest:
 		response, err = bothPrincipalAndInterest(request)
 	// 04-先息后本
 	case BeforeInterestAfterPrincipal:
-		response, err = beforeInterestAfterPrincipal(request)
+		err = beforeInterestAfterPrincipalPlan(repayPlanRequest, response)
 	// 05-等本等息
 	case EqualPrincipalAndInterest:
-		response, err = equalPrincipalAndInterest(request)
+		err = equalPrincipalAndInterestPlan(repayPlanRequest, response)
 	default:
 		return nil, errors.New("repay method error")
 	}
 	return response, err
+}
+func prepareGetParameter(request *Request) (repayPlanRequest, *Response, error) {
+	loanStartDateParseLocal, err := time.ParseInLocation(DATE_DASH_FORMAT, request.LoanStartDate, time.Local)
+	if err != nil {
+		return repayPlanRequest{}, nil, errors.New("loanStartDate date format error: " + err.Error())
+	}
+
+	firstRepayDate, err := getFirstRepayDate(request, loanStartDateParseLocal)
+	if err != nil {
+		return repayPlanRequest{}, nil, err
+	}
+	totalPeriodNum, err := getTotalPeriodNum(request, loanStartDateParseLocal)
+
+	err = getLoanEndDate(request, firstRepayDate)
+
+	loanEndDateParseLocal, err := time.ParseInLocation(DATE_DASH_FORMAT, request.LoanEndDate, time.Local)
+	if err != nil {
+		return repayPlanRequest{}, nil, errors.New("loanStartDate date format error: " + err.Error())
+	}
+
+	periodInterestRate := calculatePeriodInterestRate(request.InterestRate, request.LoanCycleCode)
+
+	daysInterestRate := calculateDaysInterestRate(request.InterestRate, request.DaysOfYear)
+
+	response := &Response{
+		RepayMethod:    request.RepayMethod,
+		LoanStartDate:  request.LoanStartDate,
+		LoanEndDate:    request.LoanEndDate,
+		TotalPeriodNum: totalPeriodNum,
+		LoanAmount:     request.LoanAmount,
+		InterestRate:   request.InterestRate,
+	}
+
+	return repayPlanRequest{
+		LoanAmount:              request.LoanAmount,
+		LoanStartDate:           request.LoanStartDate,
+		LoanEndDate:             request.LoanEndDate,
+		LoanCycleCode:           request.LoanCycleCode,
+		PeriodInterestRate:      periodInterestRate,
+		TotalPeriodNum:          totalPeriodNum,
+		FirstRepayDate:          firstRepayDate,
+		LoanStartDateParseLocal: loanStartDateParseLocal,
+		LoanEndDateParseLocal:   loanEndDateParseLocal,
+		RepayDay:                request.RepayDay,
+		DaysInterestRate:        daysInterestRate,
+	}, response, nil
 }
